ec47bf2e077b7970455b117874bb32f264241616
Daniel Valdivieso-M <dvaldi7@gmail.com>
2025-02-25 00:20:36 +0100

    added proyectos data and map in proyectos. Need fix

--- src/components/Portafolio.jsx
+++ src/components/Portafolio.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { proyectos } from '../data/proyectos'
 
 export const Portafolio = () => {
   return (
-    <div>Portafolio</div>
+    <div>
+
+      <h3>Portafolio</h3>
+
+      {
+        proyectos.map(proyectos => {
+          {console.log(proyectos)}
+          <article>
+            <h2>{proyectos.nombre}</h2>
+          </article>
+        })
+      }
+
+    </div>
   )
 }
---
+++ src/data/proyectos.jsx
@@ -0,0 +1,22 @@
+
+export const proyectos = [
+    {
+        "id": "GracieBarra",
+        "nombre": "Gracie Barra",
+        "url": "https://dvaldi7.github.io/Proyecto-GBA/",
+        "tecnologias": "HTML, CSS, JS", 
+    },
+    {
+        "id": "pokedex",
+        "nombre": "Pokedex",
+        "url": "https://dvaldi7.github.io/Pokedex/",
+        "tecnologias": "HTML, CSS, JS", 
+    },
+    {
+        "id": "excursion",
+        "nombre": "Excursion Web",
+        "url": "https://dvaldi7.github.io/excursion/",
+        "tecnologias": "HTML, CSS", 
+    },
+
+];
