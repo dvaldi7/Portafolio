78208e59f8d31a185f5a137b3f9443b4f4bd37e2
Daniel Valdivieso-M <dvaldi7@gmail.com>
2025-03-10 01:53:32 +0100

    changed router for hasrouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, HashRouter } from 'react-router-dom';
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
+    <HashRouter>
       <App /> 
-    </Router>
+    </HashRouter>
   </StrictMode>
 )
